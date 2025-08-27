5e281cd18aad2f2e903db551c9e313a147f36fec
Daniel <programador2@intareactiva.net.co>
2025-08-27 11:27:52 -0500

    feat: add react-photo-view dependency and implement photo viewing functionality in ResultDisplay component

--- src/components/ResultDisplay.jsx
+++ src/components/ResultDisplay.jsx
@@ -1,21 +1,26 @@
-import React from 'react';
-import { useCameraContext } from '../context/CameraContext';
+import { useCameraContext } from "../context/CameraContext";
+import { PhotoProvider, PhotoView } from "react-photo-view";
 
 const ResultDisplay = () => {
   const { capturedImage } = useCameraContext();
 
   return (
     <div className="p-6 border-t border-gray-700">
-      <h2 className="text-xl font-semibold text-center mb-4">¡Foto Capturada!</h2>
-      <div className="flex justify-center mb-4">
-        <img src={capturedImage} className="rounded-lg shadow-lg max-w-full h-auto" alt="Foto capturada con marco" />
-      </div>
-      <a 
-        href={capturedImage} 
-        download="foto-con-marco.png" 
-        className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded-lg text-lg flex items-center justify-center"
+     
+      <PhotoProvider>
+        <PhotoView src={capturedImage}>
+          <img
+            src={capturedImage}
+            className="rounded-lg"
+            alt="Foto capturada con marco"
+          />
+        </PhotoView>
+      </PhotoProvider>
+      <a
+        href={capturedImage}
+        download="foto-con-marco.png"
+        className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded-lg text-lg flex items-center justify-center mt-3"
       >
-        {/* SVG */}
         Descargar Foto
       </a>
     </div>
